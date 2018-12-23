8b2c5d1ac88cc84631d1c740957d4cad340e7822
Pitichinaccio <43654725+Pitichinaccio@users.noreply.github.com>
2018-12-23 17:13:00 +0100

    Update action-eastereggs.py

--- action-eastereggs.py
+++ action-eastereggs.py
@@ -16,9 +16,13 @@ def subscribe_intent_callback(hermes, intentMessage):
         hermes.publish_end_session(intentMessage.session_id, result_sentence)
 
     elif intentname == "bertron:Crystal":
-        result_sentence = "Herzlichen Glyckwunsch ich bin doch nicht schwanger Sie haben das settab abgeschlossen"
+        result_sentence = "Herzlichen Glyckwunsch ich bin doch nicht schwanger Sie haben das setup abgeschlossen"
         hermes.publish_end_session(intentMessage.session_id, result_sentence)
-
+        
+     elif intentname == "bertron:Schoenste":
+        result_sentence = "Frau Kynigin, Ihr seid die Schynste hier, aber Schneewittchen hinter den sieben Bergen, bei den sieben Zwergen ist noch tausend Mal schyner als Ihr"
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)       
+        
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intents(subscribe_intent_callback).start()
