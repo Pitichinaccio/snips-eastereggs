c60e6af93b0eb4d20b68f6f2064c7ce8f2aaa631
Pitichinaccio <43654725+Pitichinaccio@users.noreply.github.com>
2018-12-23 15:57:36 +0100

    Update and rename action-setGPIO.py to action-eastereggs.py

---
+++ action-eastereggs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+# CONFIG_INI = "config.ini"
+
+def subscribe_intent_callback(hermes, intentMessage):
+    intentname = intentMessage.intent.intent_name
+    if intentname == "bertron:Fuchs":
+        result_sentence = "Whoa  papapapapapapapo"
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+    elif intentname == "bertron:Crystal":
+        result_sentence = "Herzlichen Glückwunsch ich bin doch nicht schwanger. Sie haben das settab abgeschlossen"
+        hermes.publish_end_session(intentMessage.session_id, result_sentence)
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intents(subscribe_intent_callback).start()
+
